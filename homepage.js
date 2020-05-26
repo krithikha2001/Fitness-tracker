8204ca757eb891d52265f17a102f5bf672aa95dd
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-05-26 11:49:07 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -80,3 +80,32 @@ function formValid()
      return false; 
     }
 }
+  window.onload = () => {
+  let hour = 0;
+  let minute = 0;
+  let seconds = 0;
+  let totalSeconds = 0;
+  let intervalId = null;
+  function startTimer() {
+    ++totalSeconds;
+    hour = Math.floor(totalSeconds /3600);
+    minute = Math.floor((totalSeconds - hour*3600)/60);
+    seconds = totalSeconds - (hour*3600 + minute*60);
+    document.getElementById("hour").innerHTML =hour;
+    document.getElementById("minute").innerHTML =minute;
+    document.getElementById("seconds").innerHTML =seconds;
+  }
+  document.getElementById('start-btn').addEventListener('click', () => {
+    intervalId = setInterval(startTimer, 1000);
+  })
+  document.getElementById('stop-btn').addEventListener('click', () => {
+    if (intervalId)
+      clearInterval(intervalId);
+  });
+  document.getElementById('reset-btn').addEventListener('click', () => {
+     totalSeconds = 0;
+     document.getElementById("hour").innerHTML = '0';
+     document.getElementById("minute").innerHTML = '0';
+     document.getElementById("seconds").innerHTML = '0';
+  });
+}
